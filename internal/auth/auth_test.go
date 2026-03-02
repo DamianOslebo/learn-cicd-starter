f0c52873458ebb95e5f4c0c705c1d48f5996330b
DamianOslebo <damian.oslebo@gmail.com>
2026-03-02 18:22:55 -0500

    moved unit test in package

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,30 +1,29 @@
-package tests
+package auth
 
 import (
 	"net/http"
 	"testing"
 
-	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAuthorizationAPIKey(t *testing.T) {
 	var header http.Header
 	// Test empty input
-	_, err := auth.GetAPIKey(header)
-	assert.ErrorIs(t, err, auth.ErrNoAuthHeaderIncluded)
+	_, err := GetAPIKey(header)
+	assert.ErrorIs(t, err, ErrNoAuthHeaderIncluded)
 
 	req, err := http.NewRequest("", "", nil)
 	header = req.Header
 
 	// Test no apikey header
 	header.Add("Authorization", "ApiKey")
-	_, err = auth.GetAPIKey(header)
+	_, err = GetAPIKey(header)
 	assert.Error(t, err)
 
 	// Test valid header
 	header.Set("Authorization", "ApiKey zkldjasdfonaldnfajhgv")
-	key, err := auth.GetAPIKey(header)
+	key, err := GetAPIKey(header)
 	assert.NoError(t, err)
 	assert.Equal(t, key, "zkldjasdfonaldnfajhgv")
 }
